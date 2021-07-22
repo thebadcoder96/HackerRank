3e022fe90c7259428492542131804f2e9981e35e
Eminem <58310848+thebadcoder96@users.noreply.github.com>
2021-07-21 23:32:47 -0500

    Arrays edit 1

--- Arrays/minimumswaps.py
+++ Arrays/minimumswaps.py
@@ -1,13 +1,19 @@
 #https://www.hackerrank.com/challenges/minimum-swaps-2/problem
 
 arr=[2,3,4,1,5]
-sr=sorted(arr)
+#sr=sorted(arr)
 c=0
 
+# for i in range(len(arr)):
+#     if (arr[i]!=sr[i]):
+#         ix=arr.index(sr[i])
+#         arr[i],arr[ix]=arr[ix],arr[i]
+#         c+=1
 for i in range(len(arr)):
-    if (arr[i]!=sr[i]):
-        ix=arr.index(sr[i])
-        arr[i],arr[ix]=arr[ix],arr[i]
-        c+=1
+        while arr[i] != i+1:
+            temp = arr[i]-1
+            arr[i], arr[temp] = arr[temp], arr[i]
+            c+=1
+
 
 print(arr,c)
